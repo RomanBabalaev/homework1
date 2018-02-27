2f17683b5a5e5f8dcb701f01855867c0e02fc829
RomanBabalaev <36676728+RomanBabalaev@users.noreply.github.com>
2018-02-27 15:08:38 -0500

    Update and rename realisation 5.php to realisation_5.php

---
+++ realisation_5.php
@@ -0,0 +1,22 @@
+<?php
+
+$day = 10;
+switch ($day){
+    case 1:    
+	case 2:        
+	case 3:        
+	case 4:        
+	case 5: echo 'рабочий день';
+        break;
+    case 6:       
+    case 7: echo ' выходной день';
+        break;
+    default: echo 'Неизвестный день';
+}
+
+/**
+ * Created by PhpStorm.
+ * User: 1
+ * Date: 26.02.2018
+ * Time: 0:02
+ */
